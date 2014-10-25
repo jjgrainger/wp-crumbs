2027d3c9791e012cd8f1e771ecae05b5deff5770
Joseph Grainger <josephgrainger@Josephs-MacBook-Air.local>
2014-10-25 02:58:45 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,172 @@
+# WP Crumbs v1.0.0
+
+Simple Wordpress Breadcrumbs
+
+## Installation
+
+#### Install with Composer
+
+Add the package to your projects `composer.json` file. Visit [getcomposer.org](http://getcomposer.org/) more information.
+
+```json
+{
+    "require": {
+        "jjgrainger/wp-crumbs": "dev-master"
+    }
+}
+```
+
+#### Install Manually
+
+Download and include the class file into your themes `functions.php`:
+
+```php
+include_once 'path/to/Crumbs.php';
+```
+
+
+## Usage
+
+#### Basic usage
+
+Use `the_crumbs` to display the breadcrumbs in your template.
+
+```php
+the_crumbs();
+```
+
+## Options
+
+An optional array of arguments can be passed to modify the breadcrumb output.
+The defaults for each option are display below.
+
+##### Example
+
+```php
+the_crumbs(array(
+    'home' => 'Home',       // Title for the Home (front page) link
+    'blog' => 'Blog',       // Title for the blog home page
+    'seperator' => '/',     // Seperator to use between each crumb (string or false)
+    'class' => 'crumbs',    // The class(es) applied to the wrapper element ('crumbs', 'nav crumbs')
+    'element' => 'nav'      // The HTML element to use (div, nav, ol, ul)
+));
+```
+
+##### Output
+
+```html
+<nav class="crumbs">
+    <a href="http://site.com/">Home</a>
+    <span class="sep">/</span>
+    <a href="http://site.com/blog/">Blog</a>
+    <span class="sep">/</span>
+    <span>Single Post Title</a>
+</nav>
+```
+
+
+## Advanced usage
+
+#### get_crumbs()
+
+Use `get_crumbs()` to retrieve an array of crumbs. Each crumb has a `title` and `url`.
+
+##### example
+
+```php
+$breadcrumbs = get_crumbs();
+
+print_r($breadcrumbs);
+```
+
+##### output
+```php
+Array(
+    [0] => Array(
+        [title] => "Home"
+        [url] => http://site.com/
+    )
+    [1] => Array(
+        [title] => "Blog"
+        [url] => "http://site.com/blog/"
+    )
+    [2] => Array(
+        [title] => "My First Post"
+        [url] => false
+    )
+)
+```
+
+You can modify the returned array and/or create a function to create an output of your choosing.
+
+`get_crumbs` accepts the same aguments as `the_crumbs`.
+
+#### Filters
+
+You can further modify the crumbs array using a filter on `get_crumbs`. This will also effect the output of `the_crumbs`.
+
+##### example
+
+```php
+
+// Example: modify title for a custom post type
+function modify_crumbs($crumbs) {
+
+    // if on events archive change title to shows
+    if(is_post_type_archive('event') || is_singular('event')) {
+        for($i = 0; $i < count($crumbs); $i++) {
+            if($crumbs[$i]['title'] === 'Events') {
+                $crumbs[$i]['title'] = "Shows";
+            }
+        }
+    }
+
+    return $crumbs;
+}
+
+add_filter('get_crumbs', 'modify_crumbs');
+```
+
+#### array_insert()
+
+`array_insert()` is a function that allows you to insert a new element into an array at a specific index.
+[You can see a gist of it here](https://gist.github.com/jjgrainger/845271930a319079b74b).
+
+when modifying the crumb trail you can add new crumbs at specific points.
+
+##### example
+
+```php
+// Example: add post type archive on taxonomy archive page
+function modify_crumbs($crumbs) {
+
+    // if on the events category archive page
+    if(is_tax('event-categories')) {
+
+        // create a new crumb
+        $crumb = array(
+            'title' => "Shows",
+            'url' => site_url('/shows')
+        );
+
+        // add the new crumb at the index of 1
+        $crumbs = array_insert($crumbs, $crumb, 1);
+    }
+
+    return $crumbs;
+
+}
+
+add_filter('get_crumbs', 'modify_crumbs');
+```
+
+## Notes
+
+* Licensed under the [MIT License](https://github.com/jjgrainger/wp-crumbs/blob/master/LICENSE)
+* Maintained under the [Semantic Versioning Guide](http://semver.org)
+
+## Author
+
+**Joe Grainger**
+* [http://jjgrainger.co.uk](http://jjgrainger.co.uk)
+* [http://twitter.com/jjgrainger](http://twitter.com/jjgrainger)
---
+++ src/Crumbs.php
@@ -0,0 +1,344 @@
+<?php
+/*
+    Crumbs
+    Simple Worpdress Breadcrumbs
+    http://github.com/jjgrainger/wp-crumbs/
+
+    @author     jjgrainger
+    @url        http://jjgrainger.co.uk
+    @version    1.0.0
+    @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ */
+
+Class Crumbs {
+
+    /*
+        @var  array  $defaults
+        default options
+    */
+    var $breadcrumbs = array();
+
+
+
+    /*
+        @var  array  $defaults
+        default options
+    */
+    var $defaults = array(
+        'home' => 'Home',
+        'blog' => 'Blog',
+        'blog_url' => '',
+        'seperator' => '/',
+        'class' => 'crumbs',
+        'element' => 'nav'
+    );
+
+
+
+    /*
+        @var  array  $options
+        an array of merged user defined options with defaults
+    */
+    var $options = array();
+
+
+
+    /*
+        @function __contructor(@options)
+
+        @param  array   $options            User submitted options
+    */
+
+    function __construct($options = array()) {
+
+        // is blog/front page
+        if(get_option('show_on_front') == 'page')  {
+            $this->defaults['blog_url'] = get_permalink(get_option('page_for_posts'));
+        } else {
+            $this->defaults['blog_url'] = bloginfo('url');
+        }
+
+        // merge options
+        $this->options = array_replace_recursive($this->defaults, $options);
+
+        // start making crumbs
+        $this->do_breadcrumbs();
+
+    }
+
+
+
+    /*
+        @function the_breadcrumbs()
+        outputs breadcrumb html
+    */
+    function the_breadcrumbs() {
+
+        $breadcrumbs = $this->get_breadcrumbs();
+        $count = count($breadcrumbs);
+        $i = 1;
+        $el = $this->options['element'];
+
+        // start the element
+        $html = sprintf('<%s class="%s">', $el, $this->options['class']);
+
+        foreach($breadcrumbs as $crumb) {
+
+            // if a list wrap each item in a <li>
+            if($el === 'ul' || $el === 'ol') {
+                $html .= '<li>';
+            }
+
+            // if crumb has a url make a link
+            if($crumb['url']) {
+                $html .= sprintf('<a href="%s">%s</a>', $crumb['url'], $crumb['title']);
+
+            // otherwise make a span
+            } else {
+                $html .= sprintf('<span>%s</a>', $crumb['title']);
+            }
+
+            // add seperators
+            if($i < $count && !empty($this->options['seperator'])) {
+                $html .= sprintf('<span class="sep">%s</span>', $this->options['seperator']);
+            }
+
+            // if list, close <li> tag
+            if($el === 'ul' || $el === 'ol') {
+                $html .= '</li>';
+            }
+
+            $i++;
+
+        }
+
+        // close the element
+        $html .= sprintf('</%s>', $el);
+
+        echo $html;
+    }
+
+
+
+    /*
+        @function get_breadcrumbs()
+        returns breadcrumb array
+    */
+    function get_breadcrumbs() {
+        $breadcrumbs = $this->breadcrumbs;
+        $breadcrumbs = apply_filters('get_breadcrumbs', $breadcrumbs);
+        return $breadcrumbs;
+    }
+
+
+
+    /*
+        @function add_breadcrumb(@title, @url)
+        Adds a crumb to the breadcrumb array
+
+        @param  string  $title          title for crumb link
+        @param  string  $url            The crumb url
+
+    */
+    function add_breadcrumb($title, $url = false) {
+        $this->breadcrumbs[] = array(
+            'title' => $title,
+            'url' => $url
+        );
+    }
+
+
+
+    /*
+        @function do_breadcrumbs()
+        Create breadcrumbs
+    */
+    function do_breadcrumbs() {
+
+        // if not on the front page, add a home crumb
+        if(!is_front_page()) {
+            $this->add_breadcrumb($this->options['home'], site_url('/'));
+        }
+
+        // for pages
+        if(is_page() && !is_front_page()) {
+
+            // get ancestors and add them to the trail
+            $ancestors = get_ancestors(get_the_ID(), 'page');
+
+            if(count($ancestors) > 0) {
+                foreach($ancestors as $page_id) {
+                    $this->add_breadcrumb(get_the_title($page_id), get_permalink($page_id));
+                }
+            }
+
+            // get this page title
+            $this->add_breadcrumb(get_the_title());
+        }
+
+        // if is blog archive
+        if(is_home()) {
+            $this->add_breadcrumb($this->options['blog']);
+        }
+
+        // if a category archive
+        if(is_category()) {
+
+            $this->add_breadcrumb($this->options['blog'], $this->options['blog_url']);
+
+            // get current category
+            $cat = get_the_category();
+            $cat = $cat[0];
+            $cat_link = esc_url(get_category_link($cat->term_id));
+
+            $this->add_breadcrumb(single_cat_title('', false));
+        }
+
+        // if tag archive
+        if(is_tag()) {
+
+            $tag_id = get_query_var('tag_id');
+            $url = get_tag_link($tag_id);
+
+            $this->add_breadcrumb(single_tag_title('', false));
+        }
+
+        // single post
+        if(is_singular('post')) {
+
+            // add the archive to breadcrumbs
+            $this->add_breadcrumb($this->options['blog'], $this->options['blog_url']);
+            $this->add_breadcrumb(get_the_title());
+
+        }
+
+
+        // post types
+        if(!is_singular('page') && !is_singular('post')) {
+
+            if(is_post_type_archive()) {
+                $this->add_breadcrumb(post_type_archive_title('', false));
+            }
+
+            if(is_tax()) {
+                $taxonomy = get_query_var('taxonomy');
+                $term = get_term_by('slug', get_query_var($taxonomy), $taxonomy);
+                $tax_name = $term->name;
+
+                $this->add_breadcrumb($tax_name);
+            }
+
+            if(is_singular()) {
+
+                $post_type = get_post_type(get_the_ID());
+                $obj = get_post_type_object($post_type);
+
+                $this->add_breadcrumb($obj->labels->all_items, get_post_type_archive_link($post_type));
+                $this->add_breadcrumb(get_the_title());
+            }
+        }
+    }
+
+}
+
+
+
+/*
+    Functions
+ */
+
+/*
+    @function the_crumbs($options)
+    outputs breadcrumb html
+
+    @param  array  $options     an array of options for crumbs
+ */
+
+if(!function_exists('the_crumbs')) {
+
+    function the_crumbs($options = array()) {
+        $Crumbs = new Crumbs($options);
+        $Crumbs->the_breadcrumbs();
+    }
+
+}
+
+
+
+/*
+    @function get_crumbs($options)
+    returns the array of crumbs
+
+    @param  array  $options     an array of options for crumbs
+ */
+if(!function_exists('get_crumbs')) {
+
+    function get_crumbs($options = array()) {
+        $Crumbs = new Crumbs($options);
+        return $Crumbs->get_breadcrumbs();
+    }
+
+}
+
+
+
+/*
+    @function array_insert()
+    insert an element into an array at a specific position
+
+    @param  array  $array      the array to add an element to
+    @param  any    $element    the element to add to the array
+    @param  int    $position   the position in the array to add the element
+
+ */
+
+if(!function_exists('array_insert')) {
+
+    function array_insert($array, $element, $position) {
+
+        // if the array is empty just add the element to it
+        if(empty($array)) {
+
+            $array[] = $element;
+
+        // if the position is a negative number
+        } elseif(is_numeric($position) && $position < 0) {
+
+            // if negative position after count
+            if(count($array) + $position < 0) {
+                $position = 0;
+            } else {
+                $position = count($array) + $position;
+            }
+
+            // try again with a positive position
+            $array = array_insert($array, $element, $position);
+
+        // if array position already set
+        } elseif(isset($array[$position])) {
+
+            // split array into two parts
+            $split1 = array_slice($array, 0, $position, true);
+            $split2 = array_slice($array, $position, null, true);
+
+            // add new array element at between two parts
+            $array = array_merge($split1, array($position => $element), $split2);
+
+        // if position not set add to end of array
+        } elseif(is_null($position)) {
+
+            $array[] = $element;
+
+        // if the position is not set
+        } elseif(!isset($array[$position])) {
+            $array[$position] = $element;
+        }
+
+        // clean up indexes
+        $array = array_values($array);
+
+        return $array;
+
+    }
+
+}
